67932de155c2ad3c426ab748d0efb3f7dda6adda
Hank Liu <hank33liu@gmail.com>
2022-01-06 00:07:45 -0800

    Completed first pass of RPS game. Game runs properly in console only.

---
+++ myscripts.js
@@ -0,0 +1,78 @@
+//Returns random integer inclusive between 0 and max
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+//Returns Rock, Paper, or Scissors at an even chance
+function computerPlay() {
+    throwNum = getRandomInt(3) + 1
+    if (throwNum === 1) {return 'Rock';}
+    else if (throwNum === 2) {return 'Paper';}
+    else {return 'Scissors';}
+}
+
+//Define the inuts to playRound
+let playerSelection = "rock";
+let computerSelection = computerPlay();
+
+//Takes two consts and outputs whether the player wins or loses vs the computer
+function playRound(playerSelection, computerSelection) {
+    // Result messages
+    let msgWin = `You win! ${playerSelection} beats ${computerSelection}!`;
+    let msgLoss = `You lose (my condolences). ${computerSelection} beats ${playerSelection}.`;
+    let msgTie = `It's a tie! You both chose ${playerSelection}!`;
+
+    //Eliminate tie messages
+    //if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {return msgTie;}
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {return 2;}
+
+    //Eliminate win/loss messages
+    /*if (playerSelection.toUpperCase() === "ROCK") {
+        if (computerSelection === 'Paper') {return msgWin;}
+        else {return msgLoss}
+    } else if (playerSelection.toUpperCase() === "PAPER") {
+        if (computerSelection === 'Rock') {return msgWin;}
+        else {return msgLoss}
+    } else if (playerSelection.toUpperCase() === "SCISSORS") {
+        if (computerSelection === 'Paper') {return msgWin;}
+        else {return msgLoss}
+    } */
+     if (playerSelection.toUpperCase() === "ROCK") {
+        if (computerSelection === 'Paper') {return 1;}
+        else {return 0;}
+    } else if (playerSelection.toUpperCase() === "PAPER") {
+        if (computerSelection === 'Rock') {return 1;}
+        else {return 0;}
+    } else if (playerSelection.toUpperCase() === "SCISSORS") {
+        if (computerSelection === 'Paper') {return 1;}
+        else {return 0;}
+    }
+
+}
+
+//Game that plays 5 rounds. Tracks current score, winner of each round, and ultimate winner.
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt(`Round ${i+1}: Rock, Paper, or Scissors?`);
+        computerSelection = computerPlay();
+
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result === 0) {
+            computerScore++;
+            console.log(`The machines won Round ${i+1}...`)
+        } else if (result === 1) {
+            playerScore++;
+            console.log(`You won Round ${i+1}!`);
+        } else if (result === 2) {
+            console.log(`Round ${i+1} was a tie!`);
+        }
+    }
+
+    if (playerScore === computerScore) {console.log("The game was a tie!");}
+    else if (playerScore < computerScore) {console.log("You lost the game...");}
+    else if (playerScore > computerScore) {console.log("You won the game!");}
+}
